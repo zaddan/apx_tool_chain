ea7acf319141482763a790be24caa3327e100288
zaddan <behzadboro@gmail.com>
2016-09-09 14:34:16 -0500

    minor changes in tool_shed

--- src/python_files/tool_shed.py
+++ src/python_files/tool_shed.py
@@ -1,9 +1,10 @@
 from send_email import *
 from test_benchmark import *
 from test_benchmark_4_input_dep import *
-
+import time
 
 def run_a_tool(benchmark, bench_suit_name, root_folder, heuristic_intensity1, heuristic_intensity2,tool_type):
+    start_time = time.time() 
     subject = "starting a run"
     body = "start the test_benchmark with benchmark: " + benchmark + " heuristic_intensity1:" + heuristic_intensity1 + " heuristic_intensity2:" + heuristic_intensity2 + " tool_type:" + tool_type
 #     send_email("behzadboro@gmail.com", "+1mastermind+", "behzadboro@gmail.com", subject, body)
@@ -13,8 +14,12 @@ def run_a_tool(benchmark, bench_suit_name, root_folder, heuristic_intensity1, he
     if (tool_type == "various_inputs"):
         run_test_bench_mark_4_input_dep(benchmark, root_folder, bench_suit_name,  heuristic_intensity1, heuristic_intensity2)
 
+    end_time = time.time() 
     config_addr = open("config.txt", "w")
-    config_addr.write("test_benchmark with benchmark: " + benchmark + " heuristic_intensity1:" + heuristic_intensity1 + " heuristic_intensity2:" + heuristic_intensity2 + " tool_type:" + tool_type)
+    config_addr.write("benchmark: " + benchmark + "\n") 
+    config_addr.write("heuristic_intensity1:" + heuristic_intensity1 + " heuristic_intensity2:" + heuristic_intensity2 + "\n")
+    config_addr.write("tool_type: " + tool_type + "\n")
+    config_addr.write("run duration(minutes): " + str((end_time - start_time)/60))
     config_addr.close() 
 #     os.system("python make_backup.py " + benchmark + "  "  + tool_type)
 
