b9df615b3614fec43e4af6498ccb10ed428d586f
zaddan <behzadboro@gmail.com>
2016-11-02 17:56:35 -0500

    test_operator properly set up

--- test_operators/src/PySrc/test_verilog_vs_c.py
+++ test_operators/src/PySrc/test_verilog_vs_c.py
@@ -16,16 +16,21 @@ import os
 
 #for floating
 #for truncation
-#os.system("make");
-#os.system("cp ~/behzad_local/verilog_files/apx_operators/float_ops_apx/TRUNCATION.txt .")
-#os.system("./simple . out ../operator_sample.txt")
-#os.system("python get_elements_and_compare_truncation.py")
+os.chdir("../../build/")
+os.system("make");
+#
+os.chdir("../src/PySrc")
+os.system("cp ~/behzad_local/verilog_files/apx_operators/float_ops_apx/TRUNCATION.txt .")
+os.system("../../build/simple . out ../../operator_sample.txt")
+os.system("python get_elements_and_compare_truncation.py")
 #
 
 #for rounding
-os.system("make");
-os.system("cp ~/behzad_local/verilog_files/apx_operators/float_ops_apx/BT_RND.txt .")
-os.system("../../build/simple . out ../..//operator_sample.txt")
-os.system("python get_elements_and_compare_rnd.py")
+#os.chdir("../../build/")
+#os.system("make");
+#os.chdir("../src/PySrc")
+#os.system("cp ~/behzad_local/verilog_files/apx_operators/float_ops_apx/BT_RND.txt .")
+#os.system("../../build/simple . out ../..//operator_sample.txt")
+#os.system("python get_elements_and_compare_rnd.py")
 print colored("test passed", 'green');
 
